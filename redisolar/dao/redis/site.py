152cdbe69e57c58cbe181ff4120d0183b98d5877
IdiotCoffee <ishaanmavinkurve16@gmail.com>
2026-06-03 17:54:00 +0530

    Challenge - 1: making the find_all function

--- redisolar/dao/redis/site.py
+++ redisolar/dao/redis/site.py
@@ -1,9 +1,8 @@
 from typing import Set
 
-from redisolar.models import Site
-from redisolar.dao.base import SiteDaoBase
-from redisolar.dao.base import SiteNotFound
+from redisolar.dao.base import SiteDaoBase, SiteNotFound
 from redisolar.dao.redis.base import RedisDaoBase
+from redisolar.models import Site
 from redisolar.schema import FlatSiteSchema
 
 
@@ -12,11 +11,12 @@ class SiteDaoRedis(SiteDaoBase, RedisDaoBase):
 
     This class allows persisting (and querying for) Sites in Redis.
     """
+
     def insert(self, site: Site, **kwargs):
         """Insert a Site into Redis."""
         hash_key = self.key_schema.site_hash_key(site.id)
         site_ids_key = self.key_schema.site_ids_key()
-        client = kwargs.get('pipeline', self.redis)
+        client = kwargs.get("pipeline", self.redis)
         client.hset(hash_key, mapping=FlatSiteSchema().dump(site))
         client.sadd(site_ids_key, site.id)
 
@@ -36,9 +36,18 @@ class SiteDaoRedis(SiteDaoBase, RedisDaoBase):
 
     def find_all(self, **kwargs) -> Set[Site]:
         """Find all Sites in Redis."""
-        # START Challenge #1
-        # Remove this line when you've written code to build `site_hashes`.
-        site_hashes = []  # type: ignore
-        # END Challenge #1
-
+        # # START Challenge #1
+        # # Remove this line when you've written code to build `site_hashes`.
+        # site_hashes = []  # type: ignore
+        # # END Challenge #1
+        client = self.redis
+        # get all the site ids
+        site_ids_key = self.key_schema.site_ids_key()
+        sites = client.smembers(site_ids_key)
+        site_hashes = []
+        # iterate and add the hashes...
+        for site_id in sites:
+            hash_key = self.key_schema.site_hash_key(site_id)
+            site_hash = self.redis.hgetall(hash_key)
+            site_hashes.append(site_hash)
         return {FlatSiteSchema().load(site_hash) for site_hash in site_hashes}
